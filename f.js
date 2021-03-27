bbf64b38d824f31ca7f3e9075b5dcd11072a5eed
unknown <pushpitarathore2106@gmail.com>
2021-03-27 19:11:23 +0530

    2march

---
+++ f.js
@@ -0,0 +1,12 @@
+console.log("my name is pushpita rathore");
+var a=8;
+let b=7;
+console.log(a+b);
+//function definition
+function fun1(){
+    console.log("good evening");
+}
+//function calling
+fun1();
+fun1();
+fun1();
