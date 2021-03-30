af4be610fe6e76a8826a47d3d3e609ab5d64ed75
unknown <pushpitarathore2106@gmail.com>
2021-03-30 12:32:54 +0530

    local variable

--- b.js
+++ b.js
@@ -1,8 +1,17 @@
-var a=20;      //a and b are global variables
-let b=20;
-//function definition
+//function decleration
 function fun1(){
-    console.log(a+b);
+    let name ="pushpita";     //here name is local variable
+    console.log("hello" + name);
 }
 //function calling
 fun1();
+
+//function declaration
+function fun2(){
+    var a=5;              //here a, b and c are local variables
+    const b=5;
+    var c=a+b;
+    console.log("addition is:-" + c);
+}
+//function calling
+fun2();
