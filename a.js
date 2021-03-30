b5aaf0777d68e6d38f8513b4c3a541616ddb49cf
unknown <pushpitarathore2106@gmail.com>
2021-03-30 08:46:13 +0530

    a.js

--- a.js
+++ a.js
@@ -1,55 +1,7 @@
-console.log("hello world");
+var a=8;
+let b=5;
+console.log("addition is:-" + (a+b));
 
-var a,b;
-a=3;
-b=5;
-console.log(a+b);
-let c=7;
-console.log(a*b*c);
-//function declaration
-function hello(){
-    console.log("hello world");
-    console.log("my name is pushpita");
-}
-hello();
+console.log('addition is:-' + (a+b));
 
-function functionNameA(){
-    console.log("hello from a");
-}
-functionNameA();
-function get(){
-    console.log(a+b+c);
-}
-get();
-function functionnameB(){
-    console.log(c-b);
-}
-functionnameB();
-function functionnameC(){
-    console.log(a+c);
-}
-functionnameC();
-function functionnameD() {
-  console.log("pushpita rathore");  
-}
-functionnameD();
-function functionnameE(){
-    console.log("good morning");
-}
-functionnameE();
-function functionnameF(){
-    console.log(c-b);
-}
-functionnameF();
-function functionnameG(){
-    console.log(a+b*c);
-}
-functionnameG();
-function fuctionnameH(){
-    console.log(b*c);
-}
-functionnameG();
-function fuctionnameI(){
-    console.log(c%b);
-}
-fuctionnameI();
+console.log(`addition is:- ${a+b}`);
