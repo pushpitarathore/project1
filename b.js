d92f032d29798b780ee342af679a66774729ae21
unknown <pushpitarathore2106@gmail.com>
2021-03-30 08:51:11 +0530

    global varible declaration

--- b.js
+++ b.js
@@ -1,6 +1,8 @@
-console.log("hello world");
-const a=6;
-let b =8;
-var c=9;
-console.log(a+b);
-console.log(a+b+c);
+var a=20;      //a and b are global variables
+let b=20;
+//function definition
+function fun1(){
+    console.log(a+b);
+}
+//function calling
+fun1();
