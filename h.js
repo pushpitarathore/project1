584274cb233e461a11101ce85fff1f7b36d94733
unknown <pushpitarathore2106@gmail.com>
2021-03-31 08:23:31 +0530

    class

--- h.js
+++ h.js
@@ -1,4 +1,20 @@
-let a =6;
-var b=3;
-const c=8;
-console.log (a+b+c);
+//class declaration
+class MyClass{
+    //properties
+
+    //constructer
+
+    //method
+    myFunction1(n){
+        console.log(`hello ${n} from myFunction1`);
+        this.myFunction2();
+    }
+    myFunction2(){
+        console.log("hello from myFunction2");
+    }
+
+}  //class definition end
+//let objectname = new classname()
+let y = new MyClass();
+//object.member
+y.myFunction1("pushpita"); 
