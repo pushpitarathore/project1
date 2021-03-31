747f5c512fcb7e830c7806a069d863fa4557217c
unknown <pushpitarathore2106@gmail.com>
2021-03-31 08:37:49 +0530

    class2

--- i.js
+++ i.js
@@ -1,1 +1,25 @@
-console.log("hello pushpita");
+//class decleration
+//pascal case
+class myFirstClass{
+    //properties
+
+    //constructer
+
+    //method
+    myFunction1(){
+        console.log("hello everbody");
+    }
+    myFunction2(){
+        console.log("good morning");
+        this.myFunction3();
+    }
+    myFunction3(){
+        console.log("pushpita rathore");
+    }
+
+}   //class definition end
+//let objectname= new classname();
+let x = new myFirstClass();
+//object.member
+x.myFunction1();
+x.myFunction2();
