f8184f7a1253b790082b5e2105560e54b6ba852d
unknown <pushpitarathore2106@gmail.com>
2021-04-01 08:51:39 +0530

    class

--- l.js
+++ l.js
@@ -1,1 +1,37 @@
-console.log("namrata rathore");
+//class definition
+class A {
+    //property
+
+    //constructer
+    constructer() {
+
+    }
+
+
+    //method
+    myfun1() {
+        console.log("hello from myFun1 from class A");
+
+    }
+
+
+} //class a end
+class B extends A{
+    //property
+
+    //constructer
+    constructer(){
+        super();
+
+    }
+
+
+    //method
+    myFun2(){
+        console.log("hello from myFun2 from class B");
+    }
+} //class b defintion end 
+//let object = new classname();
+let obj = new B();
+obj.myfun1();
+obj.myfun2();
