0a1f0c686cea8a2395630f3ab85dc8907f024cb5
unknown <pushpitarathore2106@gmail.com>
2021-04-01 08:32:28 +0530

    myclass

--- k.js
+++ k.js
@@ -1,21 +1,28 @@
 //class definition
-class Pushpita {
-    //properties
+class MyClass {
+    //1. property 
+        name='';
+    
 
-    //constructer
+    //2. constructer
+    constructer() {
+        this.name ='morning';
+        
 
-    //method
-    hello(fname, lname) {
-        console.log("hello " + fname + lname);
+
     }
-    hello("pushpita ", "rathore");
 
-     fun2(x, y = 10) {
-    console.log(x + y);
-}
-fun2(34,);
-} //class decleration end
-//let objectname = new classname
-let a = new Pushpita();
-//object.member
-a.hello();
+    //3. method
+    myFun1(n) {     //n is formal argument
+        console.log(`hello ${n}`);
+        this.myFunction2();
+    }
+    myFunction2() {
+        console.log(`good ${this.name} `);
+    }
+}   //class definition end
+//create an external object of the class
+//let object = new classname();
+let o = new MyClass();
+o.myFun1("pushpita");
+
